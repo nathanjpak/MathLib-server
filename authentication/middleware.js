c3daf18e5a145aa700b5ce36d8b6f20f9c3c7f8e
nathanjpak <nathan.j.pak@gmail.com>
2022-10-21 12:13:33 -0700

    Fix status in authentication failure

--- authentication/middleware.js
+++ authentication/middleware.js
@@ -1,6 +1,6 @@
 const authenticateRequest = function(req, res, next) {
   if (!req.isAuthenticated()) {
-    res.header(401).send("No active session");
+    res.status(401).send("No active session");
   } else {
     next();
   }
